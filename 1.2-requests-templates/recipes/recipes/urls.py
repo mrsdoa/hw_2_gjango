b483ef1d35bb433944372346d50ffcf90fbba34e
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-04-20 19:36:21 +0300

    Add files via upload

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
-
+from calculator.views import recipe
 urlpatterns = [
     # здесь зарегистрируйте вашу view-функцию
+    path('<recipe>/', recipe),
 ]
